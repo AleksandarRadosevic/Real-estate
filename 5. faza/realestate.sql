7374128f45b46b3c67b28cd6ef8df46053d06213
danilovucinic5 <danilovucinic8@gmail.com>
2021-05-28 17:34:31 +0200

    DodaniTagoviUBazi

--- 5. faza/realestate.sql
+++ 5. faza/realestate.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 22, 2021 at 06:28 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Generation Time: May 28, 2021 at 03:31 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -20,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `realestate`
 --
-CREATE DATABASE IF NOT EXISTS `realestate` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `realestate`;
 
 -- --------------------------------------------------------
 
@@ -46,18 +45,18 @@ CREATE TABLE IF NOT EXISTS `administrator` (
 DROP TABLE IF EXISTS `advertisement`;
 CREATE TABLE IF NOT EXISTS `advertisement` (
   `IdOwner` int(11) DEFAULT NULL,
-  `IdAd` int(11) NOT NULL,
+  `IdAd` int(11) NOT NULL AUTO_INCREMENT,
   `TimePosted` binary(8) DEFAULT NULL,
   `Price` int(11) DEFAULT NULL,
   `Topic` varchar(20) DEFAULT NULL,
-  `IdType` int(11) NOT NULL,
   `Size` char(18) DEFAULT NULL,
   `Address` char(18) DEFAULT NULL,
   `IdPlace` int(11) NOT NULL,
   `Description` char(18) DEFAULT NULL,
+  `Purpose` varchar(20) NOT NULL,
+  `RealEstateType` varchar(30) NOT NULL,
   PRIMARY KEY (`IdAd`),
   KEY `R_8` (`IdOwner`),
-  KEY `R_11` (`IdType`),
   KEY `R_12` (`IdPlace`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -69,11 +68,18 @@ CREATE TABLE IF NOT EXISTS `advertisement` (
 
 DROP TABLE IF EXISTS `agency`;
 CREATE TABLE IF NOT EXISTS `agency` (
-  `IdA` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Name` varchar(20) DEFAULT NULL,
   `AverageMark` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`IdA`)
+  PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `agency`
+--
+
+INSERT INTO `agency` (`Id`, `Name`, `AverageMark`) VALUES
+(33, 'fsadfsdafa', '0.00');
 
 -- --------------------------------------------------------
 
@@ -151,13 +157,35 @@ CREATE TABLE IF NOT EXISTS `mark` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `municipality`
+--
+
+DROP TABLE IF EXISTS `municipality`;
+CREATE TABLE IF NOT EXISTS `municipality` (
+  `IdMunicipality` int(11) NOT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  `City` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`IdMunicipality`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `municipality`
+--
+
+INSERT INTO `municipality` (`IdMunicipality`, `Name`, `City`) VALUES
+(1, 'Cukarica', 'Beograd'),
+(2, 'Palilula', 'Beograd');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `picture`
 --
 
 DROP TABLE IF EXISTS `picture`;
 CREATE TABLE IF NOT EXISTS `picture` (
   `IdPicture` int(11) NOT NULL,
-  `Url` blob,
+  `Url` blob DEFAULT NULL,
   `Width` int(11) DEFAULT NULL,
   `Height` int(11) DEFAULT NULL,
   `IdAd` int(11) DEFAULT NULL,
@@ -200,12 +228,18 @@ CREATE TABLE IF NOT EXISTS `privilege` (
 
 DROP TABLE IF EXISTS `privilegeduser`;
 CREATE TABLE IF NOT EXISTS `privilegeduser` (
-  `IdP` int(11) NOT NULL,
-  `UMCN` varchar(13) DEFAULT NULL,
+  `Id` int(11) NOT NULL,
   `Name` varchar(30) NOT NULL,
   `Surname` varchar(30) NOT NULL,
-  PRIMARY KEY (`IdP`)
+  PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `privilegeduser`
+--
+
+INSERT INTO `privilegeduser` (`Id`, `Name`, `Surname`) VALUES
+(32, 'a1', 'a1');
 
 -- --------------------------------------------------------
 
@@ -228,11 +262,26 @@ CREATE TABLE IF NOT EXISTS `realestatetype` (
 
 DROP TABLE IF EXISTS `registereduser`;
 CREATE TABLE IF NOT EXISTS `registereduser` (
-  `IdR` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Name` varchar(30) NOT NULL,
   `Surname` varchar(30) NOT NULL,
-  PRIMARY KEY (`IdR`)
+  PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `registereduser`
+--
+
+INSERT INTO `registereduser` (`Id`, `Name`, `Surname`) VALUES
+(16, 'fdasfd', 'fdafdafda'),
+(17, 'fdafa', 'fdafda'),
+(18, 'agasggas', 'gdaga'),
+(48, 'dsad', 'sadsa'),
+(50, 'fdsafdsa', 'fdsafdsafdsa'),
+(51, 'fdsafdas', 'fdsafds'),
+(52, 'fdsafdas', 'fdsafds'),
+(53, 'fdsafdas', 'fdsafds'),
+(54, 'fdsfdsa', 'fdsafdsa');
 
 -- --------------------------------------------------------
 
@@ -271,6 +320,26 @@ CREATE TABLE IF NOT EXISTS `tags` (
   PRIMARY KEY (`IdTag`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`IdTag`, `Name`) VALUES
+(1, 'Potkrovlje'),
+(2, 'Uknjizen'),
+(3, 'Hitna prodaja'),
+(4, 'Garaza'),
+(5, 'Lift'),
+(6, 'Terasa'),
+(7, 'Podrum'),
+(8, 'Penthouse'),
+(9, 'Nova gradnja'),
+(10, 'Stara gradnja'),
+(11, 'Izvorno Stanje'),
+(12, 'Renovirano'),
+(13, 'Lux'),
+(14, 'Za renoviranje');
+
 -- --------------------------------------------------------
 
 --
@@ -285,15 +354,61 @@ CREATE TABLE IF NOT EXISTS `user` (
   `Email` varchar(20) DEFAULT NULL,
   `Phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=55 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`Id`, `Username`, `Password`, `Email`, `Phone`) VALUES
+(9, 'fdasfads', '123', 'fdsafdas', 'fdasfasd'),
 (8, 'fads', '123', 'fdasfads', 'afsdfads'),
-(0, 'Marko', 'aca', 'dsadsa', 'dsasda');
+(0, 'Marko', 'aca', 'dsadsa', 'dsasda'),
+(10, 'gagdsa', 'aca', 'fdasfdas', 'fdasfdsafdsa'),
+(11, 'gfgadga', '123', 'fdsafdsa', 'fsdafads'),
+(12, 'fdasfads', 'aca', 'fsdfsdaasd', 'fdsafsda'),
+(13, 'fdasfads', 'aca', 'fsdfsdaasd', 'fdsafsda'),
+(14, 'aca', 'aca', 'aaaaaa', 'aaaaaaa'),
+(15, 'adsa', '1111', 'sdadsa', 'dsadsaasd'),
+(16, 'aca1', '123', 'fdsaafdsa', 'fdasfdafdas'),
+(17, 'fdasfads', '123', 'fdafasfd', 'asfdas'),
+(18, 'gasdg', 'agdasgda', 'gagdsa', 'gasgdagas'),
+(19, 'gadgda', 'gdasgdas', 'sgadsgdsagdsa', 'gdasgdasgas'),
+(20, 'gadgda', 'gdasgdas', 'sgadsgdsagdsa', 'gdasgdasgas'),
+(21, 'aca123', 'aca', 'dsadsa', 'dsadsa'),
+(22, 'aca123', 'aca', 'dsadsa', 'dsadsa'),
+(23, 'fgagads', 'gadsgdas', 'gadsgdasgdsa', 'gdsagdsagsda'),
+(24, 'aggda', 'gadsgadsgda', 'gasgasdgasdg', 'gdsagdsa'),
+(25, 'fsdafdsa', 'sfdafads', 'sfdasfdas', 'fdasfdsafdas'),
+(26, 'gadgda', 'gdasgads', 'gdsgds', 'agsdagsdgsd'),
+(27, 'aca121', '111', 'aaa', 'aaaa'),
+(28, 'aca121', '111', 'aaa', 'aaaa'),
+(29, 'aca111', 'a1', 'a1', 'a1'),
+(30, 'aca111', 'a1', 'a1', 'a1'),
+(31, 'priv', 'a1', 'a1', 'a1'),
+(32, 'priv', 'a1', 'a1', 'a1'),
+(33, 'fdasfdsa', 'fsdafds', 'sfdafsdafsda', 'sfdafdsafsda'),
+(34, 'fasdfdas', 'aca', 'a', 'a'),
+(35, 'aaaa', 'aca', 'das', '06362367811111111'),
+(36, 'pera', 'pera', 'pera', '123456789'),
+(37, 'peki', 'peki', 'peki', '123456677'),
+(38, 'jovan', 'a', 'dfdafadfadfa', '063623678'),
+(39, 'ga', 'gdsagds', 'gdsagads', 'gdsadgsgdsa'),
+(40, 'fdas', 'fsad', 'fdsa', 'fasd'),
+(41, 'fdas', 'fsdaf', 'fsdasfdasfda', 'fsdasfdafsda'),
+(42, 'dsa', 'dsa', 'asdsad', 'sdasda'),
+(43, 'dsa', 'dsa', 'asdsad', 'sdasda'),
+(44, 'dsadsa', 'sdaasda', 'aaa', 'aaa'),
+(45, 'dsadsa', 'sdaasda', 'aaa', 'aaa'),
+(46, 'fdas', 'fasdasfdfsad', 'fdsafsda', 'fsdafdsadsa'),
+(47, 'fdas', 'fasdasfdfsad', 'fdsafsda', 'fsdafdsadsa'),
+(48, 'adsa', NULL, 'dsadsasad', 'sdasdasad'),
+(49, 'fda', 'fads', 'fsda', 'sfda'),
+(50, 'fdasfads', 'fads', 'fdsaf', 'dsafdsafdsa'),
+(51, 'fads', 'fsdafds', 'fdsafdsa', 'fdsafdsa'),
+(52, 'fads', 'fsdafds', 'fdsafdsa', 'fdsafdsa'),
+(53, 'fads', 'fsdafds', 'fdsafdsa', 'fdsafdsa'),
+(54, 'fsadfdsa', 'fsdaf', 'fdsafsd', 'dsfafdsafdsadfsa');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
