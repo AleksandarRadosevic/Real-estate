871fe31876263229f2acec5b9647058f9ebea598
AleksandarRadosevic <aradosevic40@gmail.com>
2021-05-31 22:36:19 +0200

    Create Agency.php

---
+++ 5. faza/RealEstate/app/Controllers/Agency.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Agency extends BaseController
+{
+	public function index()
+	{
+		// registrovani korisnik
+                echo '<h1>Agencija</h1>';
+	}
+}
