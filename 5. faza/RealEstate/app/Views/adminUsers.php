7ca6980edb4a542e4a4fa99d21a9e98d609d04fe
Luka Jušković <juskovicluka@gmail.com>
2021-06-19 22:28:52 +0200

    zavrsen header za admina i obicnog

--- 5. faza/RealEstate/app/Views/adminUsers.php
+++ 5. faza/RealEstate/app/Views/adminUsers.php
@@ -14,22 +14,22 @@
     <title>Prototip</title>
 </head>
 <body>
-    <nav >
-        <div class="logo"><h4><a  href='../index.html'>Success</a></h4></div>
-        
-        <div class="logo"id="logo2">
-            <a href='/Home' class="btn" style="background-color: rgb(33, 74, 255);">Početna stranica</a>
-            <a href='/Guest/search' class="btn" style="background-color: rgb(33, 74, 255);">Pretraga</a>
-            <a href='/Guest/Ads'class="btn" style="background-color: rgb(33, 74, 255);">Oglasi</a>
-               <a href='../Admin' id="myProf"><div class="btn btn-success">Moj profil</div></a>&nbsp;
-               <a href='../../Admin/logout' ><div class="btn btn-danger">Odjavi se</div></a>
-               </div>
+    <nav>
+        <div class="logo"><h4><a  href='/Home'>Success</a></h4></div>
+        <ul class="nav-links">
+            <li><a href='/Home'>Početna stranica</a></li>
+            <li><a href='/Guest/search'>Pretraga</a></li>
+            <li><a href='/Guest/Ads'>Oglasi</a></li>
+            <li><a href='../Admin' class='login'>Moj profil</a></li>
+            <li><a href='../Admin/logout' class='button register'>Odjavi se</a></li>    
+        </ul>
+                
         <div class="hidden-menu">
             <div class="line1"></div>
             <div class="line2"></div>
             <div class="line3"></div>
         </div>
-    </nav> 
+    </nav>
     <div class="containter">
         <main>
         <section class="glass">
